61aa4e6f056d18cb38993ee72797e0ad196a3521
malleshgugloth <malleshguglothms@gmail.com>
2025-12-15 16:57:48 +0530

    feat: initial project skeleton with maven setup

---
+++ src/main/java/com/gm/expensight/ExpensightApplication.java
@@ -0,0 +1,13 @@
+package com.gm.expensight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/expensight/ExpensightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.expensight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
